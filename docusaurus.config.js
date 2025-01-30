2c3d5947cfa936089adc82c2912d49dbd40e44fc
Borderless <113085523+inhabitants@users.noreply.github.com>
2025-01-30 00:33:08 -0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,8 +10,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'White Paper - Pinto Brasil 🐥 Helenai',
+  tagline: 'Experimentando o futuro',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
