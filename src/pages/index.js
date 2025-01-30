380db8d74415f431dc7b005622a9b7339a067826
Borderless <113085523+inhabitants@users.noreply.github.com>
2025-01-30 00:34:48 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            White Paper - Pinto Brasil 🐥 Helenai
+            White Paper
           </Link>
         </div>
       </div>
