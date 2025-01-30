11c5fe55b1c9934a4cbe9bf8fb3705908c3b55cf
Borderless <113085523+inhabitants@users.noreply.github.com>
2025-01-30 00:24:24 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            White Paper - Pinto Brasil 🐥 Helenai
           </Link>
         </div>
       </div>
