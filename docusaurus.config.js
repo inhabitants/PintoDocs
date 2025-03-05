218c781ea370a2bb1773e6982bfaf7695c151544
inhabitants <borderlessprotocol@gmail.com>
2025-03-05 15:33:05 -0500

    redirectupdate

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -142,6 +142,20 @@ const config = {
         darkTheme: prismThemes.dracula,
       },
     }),
+
+  plugins: [
+    [
+      '@docusaurus/plugin-client-redirects',
+      {
+        redirects: [
+          {
+            from: '/',
+            to: '/docs/intro',
+          },
+        ],
+      },
+    ],
+  ],
 };
 
 export default config;
